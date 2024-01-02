8f90095ad246a2eba544c65f839be8883b814162
Flame Sage <chris062689@gmail.com>
2024-01-01 20:05:52 -0500

    Initial commit.

---
+++ src/components/HelloWorld.stories.ts
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import Component from './HelloWorld.vue';
+
+const meta = {
+  title: 'Components/HelloWorld',
+  component: Component,
+  tags: ['autodocs'],
+  argTypes: {
+    size: { control: 'select', options: ['small', 'medium', 'large'] },
+    backgroundColor: { control: 'color' },
+    onClick: { action: 'clicked' },
+  },
+  args: { primary: false }, // default value
+} satisfies Meta<typeof Component>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    primary: false,
+    label: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    label: 'Button',
+    size: 'large',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    label: 'Button',
+    size: 'small',
+  },
+};
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-3xl font-bold underline text-violet-600">
+    Hello world!
+  </h1>
+</template>
